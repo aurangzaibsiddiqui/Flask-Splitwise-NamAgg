f5a6730bbb1be15ad7055b2136baa1e169cfd61f
aurangzaibsiddiqui <53447798+aurangzaibsiddiqui@users.noreply.github.com>
2019-12-19 17:01:06 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-consumer_key = '<your consumer key>'
-consumer_secret = '<your consumer secret>'
+consumer_key = 'he0Mq8p7CIpzw3Ao5WMvmmwDctlSKmyhbcgH4FmO'
+consumer_secret = 'DXmrRlTPo5Eney984lI7g0WfpJBk5SAu0MeNqL2P'
